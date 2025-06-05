f09539db91ff79effac230009c5140d0e46b10de
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-06-05 14:56:05 +0100

    Functions, Data Structures and Modules

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+
+def perform_operation(num1, num2, operation):
+    if "add" in operation:
+        return num1 + num2
+    elif "subtract" in operation:
+        return num1 - num2
+    elif "multiply" in operation:
+        return num1 * num2
+    elif "divide" in operation:
+        if num2 == 0:
+            return "Cannot divide by zero."
+        else:
+            return num1 / num2
+    else:
+        return "Enter a valid operation."
+    
